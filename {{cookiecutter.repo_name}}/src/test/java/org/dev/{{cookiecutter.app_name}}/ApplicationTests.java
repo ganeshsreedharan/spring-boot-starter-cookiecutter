0c5ef1f70e8cfd0e11d427a47f8448b3087053fa
AshamolCM <cmashamolcm@gmail.com>
2023-06-17 19:03:50 +0200

    cookie cutter

---
+++ {{cookiecutter.repo_name}}/src/test/java/org/dev/{{cookiecutter.app_name}}/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dev.cookiecutter.app_name;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
